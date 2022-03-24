2b41b0c447e91e75e36b4ca7908bdd2f7024a2d4
JeffersonCarvalhoJr <jcarvalhojr@hotmail.com>
2022-03-23 22:07:40 -0300

    trancar

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  
 })
+
+module.exports = {
+  devServer: {
+      contentBase: './appout',
+      host: '0.0.0.0', 
+      port: 88,
+      historyApiFallback: true, // is it enabled ?
+      inline: true
+  }
+}
